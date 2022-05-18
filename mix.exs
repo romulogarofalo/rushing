00142e2e616337166de6f27d33373c1f1a4b8027
romulogarofalo <romulogarofalo@gmail.com>
2022-05-18 20:31:28 -0300

    :heavy_plus_sign: add credo

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Rushing.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:nimble_csv, "~> 1.2"}
+      {:nimble_csv, "~> 1.2"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
