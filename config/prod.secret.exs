346afeb3ff6db9d4b089fe0ffb987cdb6f1ab2a1
romulogarofalo <romulogarofalo@gmail.com>
2022-05-19 00:19:15 -0300

    :rocket: prod configs

---
+++ config/prod.secret.exs
@@ -0,0 +1,14 @@
+import Config
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    enviroment varible DATABASE_URL is missing.
+    for exemple: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :rushing, Rushing.Repo,
+  adapter: Ecto.Adapter.Postgres,
+  database: "",
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 10)
