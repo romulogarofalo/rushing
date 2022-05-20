1d89fa5cb133ebfb35837a86a36e0a3950805d54
romulogarofalo <romulogarofalo@gmail.com>
2022-05-19 23:06:35 -0300

    :lipstick: run mix format

--- test/rushing/helpers/csv_test.exs
+++ test/rushing/helpers/csv_test.exs
@@ -1,14 +1,14 @@
 defmodule Rushing.Helpers.CsvTest do
-	use ExUnit.Case
+  use ExUnit.Case
   use RushingWeb.ConnCase
 
-	alias Rushing.Helpers.Csv
+  alias Rushing.Helpers.Csv
   alias Rushing.Statistics.StatisticsModel
 
   describe "download/3" do
     setup do
       inserted_data =
-        Enum.map([1,2], fn number ->
+        Enum.map([1, 2], fn number ->
           %{
             "Player" => Integer.to_string(number),
             "Team" => "JAX",
@@ -33,7 +33,7 @@ defmodule Rushing.Helpers.CsvTest do
       %{inserted_data: inserted_data}
     end
 
-    test "with all params", %{conn: conn, inserted_data: inserted_data} do
+    test "with all params", %{conn: conn} do
       conn =
         conn
         |> put_resp_content_type("text/csv")
@@ -43,8 +43,8 @@ defmodule Rushing.Helpers.CsvTest do
       {:ok, %{resp_body: resp_body}} = Csv.download(nil, nil, nil, conn)
 
       assert resp_body == """
-      Player,Team,Pos,Att/G,Att,Yds,Avg,Yds/G,Td,Lng,1st,1st%,20+,40+,FUM\r\n1,JAX,RB,2,2.0,7,3.5,7.0,6,7,0,0.0,0.0,0,0\r\n2,JAX,RB,2,2.0,7,3.5,7.0,6,7,0,0.0,0.0,0,0\r
-      """
+             Player,Team,Pos,Att/G,Att,Yds,Avg,Yds/G,Td,Lng,1st,1st%,20+,40+,FUM\r\n1,JAX,RB,2,2.0,7,3.5,7.0,6,7,0,0.0,0.0,0,0\r\n2,JAX,RB,2,2.0,7,3.5,7.0,6,7,0,0.0,0.0,0,0\r
+             """
     end
   end
 end
